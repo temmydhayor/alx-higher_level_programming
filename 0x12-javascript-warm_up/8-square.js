ebdbb607b6167a075889c4c0e4c7b0668833d9dd
temmydhayor <ttemitopealao@gmail.com>
2024-03-11 14:38:36 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const squareSize = process.argv[2];
+const mySquare = parseInt(squareSize);
+const x = 'x';
+
+if (isNaN(mySquare)) {
+	console.log('Missing size');
+} else {
+	for (let i = 0; i < mySquare; i++) {
+	}
+}
