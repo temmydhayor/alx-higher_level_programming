0ea8d722519cb9b385e0396ab859c9ebf01c0104
temmydhayor <ttemitopealao@gmail.com>
2024-03-11 13:47:48 -0700

    3-value_argument.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argsLengtth = process.argv.length;
+
+if (argsLength == 2) {
+	console.log('No arguement');
+}	else if (argsLength === 3) {
+	cconsole.log('Argument found');
+}	else {
+	console.log('Arguements found');
+}
