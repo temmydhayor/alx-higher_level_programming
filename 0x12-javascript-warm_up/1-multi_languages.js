8f006b007ccbca4e592bd7537e18d81f5bba9c70
temmydhayor <ttemitopealao@gmail.com>
2024-03-11 14:49:55 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-
-const firstLine = 'c is fun';
-const secondLine = 'python is cool';
-const thirdLine = 'JavaScript is amazing';
-console.log(firstLine);
-console.log(secondLine);
-console.log(thirdLine);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
